b5ee9a264bcc5525b3a368e5712785b97fec7f5e
Eric Rodriguez <feric.rodriguez@gmail.com>
2024-03-01 13:20:51 -0300

    get google credentials

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,46 @@
+import { useRef } from "react";
+import { useEffect } from "react";
+
+import { ubuntuApi } from "../utils/services/axiosConfig";
+import { Box } from "@mui/material";
+
 export const Login = () => {
+
+    const googleButton = useRef();
+
+    const handleCredentialResponse = async (response) => {
+        console.info('JWT GOOGLE: ', response.credential);
+        const data = {
+            token_id: response.credential
+        }
+
+        // const userResponse = await ubuntuApi.get(`/auth/google`, data);
+
+
+        // console.log(userResponse)
+    }
+
+    useEffect(() => {
+        if (window.google) {
+                window.google.accounts.id.initialize({
+                    client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+                    callback: handleCredentialResponse,
+                });
+                window.google.accounts.id.renderButton(
+                    googleButton.current,
+                    { type: 'standard', shape: 'pill', theme: "filled_black", size: "medium", text: 'signin_with' }  // customization attributes
+                );
+        }
+
+    }, [])
+
     return (
-        <div>Login</div>
+        <Box
+            sx={{
+                width: '200px'
+            }}
+        >
+            <div ref={googleButton}></div>
+        </Box>
     )
 }
