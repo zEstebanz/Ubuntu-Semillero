5f40f11e7b04f094a23812237a8ee45fd2c33d05
Flavio <flaviosalas050@gmail.com>
2024-03-01 22:30:46 -0300

    added Goals section

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,10 @@
+import React from 'react';
+import Goals from '../components/landing/goals';
 
 export const Landing = () => {
     return (
-        <div>Landing</div>
+        <div>
+            <Goals />
+        </div>
     )
 }
