fe4066dce86e2eb47bc99ff2e198f27b0d2b1bb2
Esteban <estebanoller73@gmail.com>
2024-03-03 21:30:36 -0300

    add feature/publicaciones branch

---
+++ src/components/Publications/PostCard.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { Card, CardContent, Typography, Button } from '@mui/material';
+
+function PostCard({ title, description, date }) {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleExpand = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card className="post-card">
+      <CardContent>
+        <Typography variant="h5" component="div" gutterBottom>
+          {title}
+        </Typography>
+        <Typography>
+          {date}
+        </Typography>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          className={expanded ? 'expanded' : 'collapsed'} // Aplica una clase condicional para expandir o colapsar el texto
+        >
+          {description}
+        </Typography>
+        {!expanded && (
+          <Button variant="outlined" onClick={toggleExpand}>
+            Ver más
+          </Button>
+        )}
+      </CardContent>
+    </Card>
+  );
+}
+
+export default PostCard;
