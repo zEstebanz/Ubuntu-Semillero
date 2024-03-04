7d25b0e04ad879027d2f36693aa0dc41d7edd5aa
Eric Rodriguez <feric.rodriguez@gmail.com>
2024-03-04 10:32:09 -0300

    feat: custom google button

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,46 +1,10 @@
-import { useRef } from "react";
-import { useEffect } from "react";
-
-import { ubuntuApi } from "../utils/services/axiosConfig";
-import { Box } from "@mui/material";
+import { GoogleButton } from "../components/login/GoogleButton"
 
 export const Login = () => {
 
-    const googleButton = useRef();
-
-    const handleCredentialResponse = async (response) => {
-        console.info('JWT GOOGLE: ', response.credential);
-        const data = {
-            token_id: response.credential
-        }
-
-        // const userResponse = await ubuntuApi.get(`/auth/google`, data);
-
-
-        // console.log(userResponse)
-    }
-
-    useEffect(() => {
-        if (window.google) {
-                window.google.accounts.id.initialize({
-                    client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
-                    callback: handleCredentialResponse,
-                });
-                window.google.accounts.id.renderButton(
-                    googleButton.current,
-                    { type: 'standard', shape: 'pill', theme: "filled_black", size: "medium", text: 'signin_with' }  // customization attributes
-                );
-        }
-
-    }, [])
-
     return (
-        <Box
-            sx={{
-                width: '200px'
-            }}
-        >
-            <div ref={googleButton}></div>
-        </Box>
+        <div>
+            <GoogleButton />
+        </div>
     )
 }
