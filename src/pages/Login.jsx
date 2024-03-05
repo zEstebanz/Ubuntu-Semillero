b210cc61220d3dfde22fe889a823f8ef6451be78
rodriguez.eric <feric.rodriguez@gmail.com>
2024-03-04 23:46:21 -0300

    styles: login styles applied

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,46 @@
-import { Box } from "@mui/material"
+import { Box, Container, Typography, styled } from "@mui/material"
 import { GoogleButton } from "../components/Login/GoogleButton"
+import theme from "../theme/theme"
+
+const Title = styled(Typography)(({ theme }) => ({
+    ...theme.typography.titles,
+    textAlign: 'center',
+}));
 
 export const Login = () => {
 
     return (
-        <Box
+        <Container
             sx={{
-                
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                backgroundImage: 'url("/img/login-bg.jpeg")',
+                backgroundRepeat: 'none',
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
             }}
+            maxWidth='none'
         >
-            <GoogleButton />
-        </Box>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'start',
+                    alignItems: 'center',
+                    width: 1,
+                    pt: 4,
+                    pb: 6,
+                    bgcolor: 'common.white',
+                    maxWidth: '500px'
+                }}
+                borderRadius={theme.shape.borderRadiusSmall}
+            >
+                <Title px={4} variant="h1">Ingreso Administrador</Title>
+                <img style={{ marginTop: 24, marginBottom: 64 }} className="" src="/img/logo.svg" alt="" />
+                <GoogleButton />
+            </Box>
+        </Container>
     )
 }
