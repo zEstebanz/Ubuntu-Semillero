f1ed2055b11b9dacb459f7b1f871e867be83bdb7
rodriguez.eric <feric.rodriguez@gmail.com>
2024-03-04 21:31:56 -0300

    fix: login components folder name capitalized

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material"
-import { GoogleButton } from "../components/Conflict/GoogleButton"
+import { GoogleButton } from "../components/Login/GoogleButton"
 
 export const Login = () => {
 
