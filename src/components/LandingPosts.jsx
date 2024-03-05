ebe17307e885ac138e3a4eb08404a77d3669375a
Tomas Guignet <tomasguignet9@gmail.com>
2024-03-05 11:02:25 -0300

    Post-Card styled

--- src/components/LandingPosts.jsx
+++ src/components/LandingPosts.jsx
@@ -1,13 +1,13 @@
 import { Box, Typography, Button } from "@mui/material";
+import img from "../assets/Blog.jpg";
 import PostCard from "./Publications/PostCard";
 
 function LandingPosts() {
   const posts = [
     {
       id: 1,
-      title: "1 Inversiones Éticas: Más que ganancias",
-      imageUrl:
-        "https://images.unsplash.com/photo-1542273917363-3b1817f69a2d?q=80&w=1474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Inversiones Éticas: Más que ganancias",
+      imageUrl: img,
       date: "03-03-2024",
       description:
         "Las decisiones financieras han trascendido la mera maximización del rendimiento. Actualmente, muchos inversores desean que sus decisiones reflejen sus valores éticos y morales, dando lugar a las inversiones éticas o sostenibles.Las decisiones financieras han trascendido la mera maximización del rendimiento. Actualmente, muchos inversores desean que sus decisiones reflejen sus valores éticos y morales, dando lugar a las inversiones éticas o sostenibles.",
@@ -15,7 +15,7 @@ function LandingPosts() {
     },
     {
       id: 2,
-      title: "2 Inversiones Éticas: Más que ganancias",
+      title: "Inversiones Éticas: Más que ganancias",
       imageUrl:
         "https://images.unsplash.com/photo-1542273917363-3b1817f69a2d?q=80&w=1474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       date: "03-03-2024",
@@ -25,9 +25,9 @@ function LandingPosts() {
     },
     {
       id: 3,
-      title: "3 Inversiones Éticas: Más que ganancias",
+      title: "Inversiones Éticas: Más que ganancias",
       imageUrl:
-        "https://images.unsplash.com/photo-1542273917363-3b1817f69a2d?q=80&w=1474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://www.coliseugeek.com.br/wp-content/uploads/2023/01/d9f70-clickwallpapers-lion-4k-img2-scaled-1.jpg",
       date: "03-03-2024",
       description:
         "Las decisiones financieras han trascendido la mera maximización del rendimiento. Actualmente, muchos inversores desean que sus decisiones reflejen sus valores éticos y morales, dando lugar a las inversiones éticas o sostenibles.Las decisiones financieras han trascendido la mera maximización del rendimiento. Actualmente, muchos inversores desean que sus decisiones reflejen sus valores éticos y morales, dando lugar a las inversiones éticas o sostenibles.Las decisiones financieras han trascendido la mera maximización del rendimiento. Actualmente, muchos inversores desean que sus decisiones reflejen sus valores éticos y morales, dando lugar a las inversiones éticas o sostenibles.Las decisiones financieras han trascendido la mera maximización del rendimiento",
@@ -44,7 +44,7 @@ function LandingPosts() {
       <Typography variant="paragraphs" fontWeight={600} marginBottom={0.3}>
         Publicaciones
       </Typography>
-      <Typography variant="subtitles" fontWeight={600} marginBottom={3}>
+      <Typography fontSize={22} fontWeight={600} marginBottom={3}>
         Finanzas con impacto
       </Typography>
 
@@ -61,6 +61,7 @@ function LandingPosts() {
             title={post.title}
             description={post.description}
             date={post.date}
+            imageUrl={post.imageUrl}
           />
         ))}
       </Box>
--- src/components/Publications/PostCard.jsx
+++ src/components/Publications/PostCard.jsx
@@ -1,14 +1,24 @@
 import { useState } from "react";
-import {
-  Card,
-  CardContent,
-  Typography,
-  Button,
-  CardMedia,
-} from "@mui/material";
+import { Card, CardContent, Typography, Button } from "@mui/material";
+import leftArrow from "../../assets/leftArrow.png";
+import rightArrow from "../../assets/rightArrow.png";
 
-function PostCard({ title, description, date }) {
+function PostCard({ title, description, date, imageUrl }) {
   const [expanded, setExpanded] = useState(false);
+  const images = [imageUrl, imageUrl, imageUrl];
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentSlide((prev) => (prev + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prev) => (prev - 1 + images.length) % images.length);
+  };
+
+  const goToSlide = (index) => {
+    setCurrentSlide(index);
+  };
 
   const toggleExpand = () => {
     setExpanded(!expanded);
@@ -17,39 +27,93 @@ function PostCard({ title, description, date }) {
   return (
     <Card
       className="post-card"
-      sx={{ marginBottom: "2rem", borderRadius: "1rem" }}
+      sx={{
+        marginBottom: "2rem",
+        borderRadius: "1rem",
+      }}
     >
-      <CardContent>
+      <CardContent
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          paddingBottom: "0.8rem !important",
+        }}
+      >
         <Typography
-          variant="subtitle"
+          variant="subtitles"
           fontWeight={600}
           component="div"
           gutterBottom
+          alignSelf="center"
+          marginBottom={2.5}
         >
           {title}
         </Typography>
-        <Typography variant="body2" fontWeight={600}>
+        <div className="post-image">
+          <div className="carousel">
+            <button className="left-arrow">
+              <img src={leftArrow} alt="left-arrow" />
+            </button>
+            <div className="slides">
+              {images.map((image, index) => (
+                <div
+                  key={index}
+                  className={`slide ${index === currentSlide ? "active" : ""}`}
+                >
+                  <img src={image} alt={`Slide ${index + 1}`} />
+                </div>
+              ))}
+            </div>
+            <button className="right-arrow">
+              <img src={rightArrow} alt="right-arrow" />
+            </button>
+          </div>
+          <div className="controls">
+            {images.map((image, index) => (
+              <button
+                key={index}
+                className={`button-group ${
+                  index === currentSlide ? "active" : ""
+                }`}
+                onClick={() => goToSlide(index)}
+              ></button>
+            ))}
+          </div>
+        </div>
+
+        {/*         <div className="post-card-image">
+          <img src={imageUrl} />
+        </div> */}
+        <Typography
+          variant="body2"
+          fontWeight={600}
+          marginTop={4}
+          marginBottom={0.6}
+          color="common.black"
+        >
           {date}
         </Typography>
         <Typography
           variant="body2"
           fontWeight={600}
-          color="text.secondary"
+          color="common.black"
           className={expanded ? "expanded" : "collapsed"} // Aplica una clase condicional para expandir o colapsar el texto
         >
           {description}
         </Typography>
-        {!expanded && (
-          <Button
-            variant="text"
-            sx={{ textTransform: "none" }}
-            onClick={toggleExpand}
-          >
-            <Typography variant="body2" fontWeight={600} color="principal">
-              {expanded ? "Ver menos" : "Ver más"}
-            </Typography>
-          </Button>
-        )}
+        <Button
+          variant="text"
+          sx={{
+            textTransform: "none",
+            alignSelf: "center",
+            marginTop: "0.8rem",
+          }}
+          onClick={toggleExpand}
+        >
+          <Typography variant="body2" fontWeight={600} color="principal">
+            {expanded ? "Ver menos" : "Ver más"}
+          </Typography>
+        </Button>
       </CardContent>
     </Card>
   );
