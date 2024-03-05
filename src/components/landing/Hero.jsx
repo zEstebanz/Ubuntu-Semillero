ffe7bb841d4a75f700044f15eb5f23947b37c18e
rodriguez.eric <feric.rodriguez@gmail.com>
2024-03-05 17:21:25 -0300

    feat: landing hero created

---
+++ src/components/landing/Hero.jsx
@@ -0,0 +1,67 @@
+import { Box, Container, Typography } from "@mui/material";
+import SearchBar from "../Publications/SearchBar"
+import styled from "@emotion/styled";
+
+const SectionTitle = styled(Typography)(({ theme }) => ({
+    ...theme.typography.subtitles,
+    textAlign: 'left',
+    fontWeight: 600,
+    fontSize: '1.125rem',
+}));
+
+const Title = styled(Typography)(({ theme }) => ({
+    ...theme.typography.titles,
+    textAlign: 'left',
+    fontWeight: 500,
+}));
+
+export const Hero = () => {
+    return (
+        <Box
+            sx={{
+                height: '488px',
+                mx: 'auto',
+                p: 0,
+                backgroundImage: 'url("/img/landing-hero-bg.jpeg")',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+            }}
+            maxWidth='sm'
+        >
+            <Container
+                sx={{
+                    width: 1,
+                    height: 1,
+                    pt: 3,
+                    px: 2,
+                    backdropFilter: 'brightness(30%)'
+                }}
+            >
+                <SearchBar />
+                <SectionTitle
+                    color='common.white'
+                    variant="h2"
+                    sx={{
+                        mt: 5
+                    }}
+                >
+                    FINANCIAMIENTO SOSTENIBLE
+                </SectionTitle>
+                <Title
+                    color='common.white'
+                    variant="h1"
+                    sx={{
+                        pr: 10,
+                        mt: {
+                            xs: 1,
+                            md: 4
+                        },
+                    }}
+                >
+                    Impulsamos el desarrollo de finanzas de impacto, liderando la transición hacia un modelo financiero sostenible
+                </Title>
+            </Container>
+        </Box>
+    )
+}
