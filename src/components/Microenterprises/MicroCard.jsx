8fc2fbc10d0696ac012c20ca45642a8b399270b6
Esteban <estebanoller73@gmail.com>
2024-03-06 12:45:32 -0300

    ready micro card styles

--- src/components/Microenterprises/MicroCard.jsx
+++ src/components/Microenterprises/MicroCard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Card, CardContent, Typography, Grid, CardMedia, IconButton } from '@mui/material';
+import { Card, CardContent, Typography, Grid, CardMedia, IconButton, Button, Divider } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
@@ -13,22 +13,14 @@ function MicroCard({ title, entity, categori, location, imageUrl, link }) {
 
   return (
     <Grid container justifyContent="center">
-
-      <Card className="post-card" sx={{
+      <Card sx={{
         width: "328px",
         height: 'fit-content',
-        top: "1200px",
-        left: "16px",
-        padding: '16px 0px 0px 0px',
         marginBottom: "16px",
         borderRadius: "16px",
         gap: "16px"
       }}>
-        <CardContent sx={{
-          width: 'hug',
-          height: 'hug',
-          gap: "8px"
-        }}>
+        <CardContent>
           <CardMedia
             component="img"
             height="144"
@@ -37,90 +29,108 @@ function MicroCard({ title, entity, categori, location, imageUrl, link }) {
             alt={title}
             sx={{
               borderRadius: "16px",
-              gap: "16px"
             }}
           />
-          <CardContent sx={{ marginLeft: '-15px' }}>
-            <Typography
-              component="div"
-              gutterBottom
-              sx={{
-                fontFamily: 'Lato',
-                fontSize: '18px',
-                fontWeight: 600,
-                lineHeight: '25px',
-                letterSpacing: '0px',
-                textAlign: 'left'
-              }}
-            >
-              {title}
-            </Typography>
+          <Typography
+            variant="h6"
+            gutterBottom
+            sx={{
+              fontFamily: 'Lato',
+              fontSize: '18px',
+              fontWeight: 600,
+              lineHeight: '25px',
+              letterSpacing: '0px',
+              textAlign: 'left',
+              marginTop: '16px'
+            }}
+          >
+            {title}
+          </Typography>
+          <Typography
+            sx={{
+              color: '#093C59',
+              fontSize: '14px',
+              fontWeight: 700,
+              lineHeight: '18px',
+              letterSpacing: '0px',
+              textAlign: 'left',
+              width: '100%',
+            }}
+          >
+            {entity}
+          </Typography>
+          <Typography
+            sx={{
+              fontSize: '14px',
+              fontWeight: 400,
+              lineHeight: '18px',
+              letterSpacing: '0px',
+              textAlign: 'left',
+              marginTop: '8px',
+              marginBottom: '16px'
+            }}
+          >
+            {categori}
+          </Typography>
+          <Typography
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              fontFamily: 'Nunito',
+              fontSize: '14px',
+              fontWeight: 400,
+              lineHeight: '20px',
+              letterSpacing: '0px',
+              textAlign: 'left',
+            }}
+          >
+            <LocationOnIcon sx={{ marginRight: '4px', color: '#093C59' }} />
+            {location}
+          </Typography>
 
-            <Typography
-              sx={{
-                color: '#093C59',
-                fontSize: '14px',
-                fontWeight: 700,
-                lineHeight: '18px',
-                letterSpacing: '0px',
-                textAlign: 'left',
-                width: '304px',
-                height: '16px',
-                top: '28px',
-                marginBottom: '10px'
-              }}
-            >
-              {entity}
-            </Typography>
+          {expanded && (
+            <>
+              <Typography color="primary" sx={{ marginTop: '16px', fontWeight: '600', marginBottom: '5px' }}>
+                Descripción del Microemprendimiento
+              </Typography>
+              <Typography variant="body2" sx={{ marginBottom: '16px' }}>
+                Promueven un modelo de agricultura sostenible, protegiendo el medio ambiente, el agua y las semillas autóctonas. Cultivan frutas, verduras, plantas medicinales y crean derivados. Editan también contenidos educativos, gestionan un banco de semillas y comercializan o intercambian excedentes.
+              </Typography>
 
-            <Typography
-              sx={{
-                width: '304px',
-                height: '16px',
-                top: '52px',
-                fontFamily: 'Lato',
-                fontSize: '14px',
-                fontWeight: 400,
-                lineHeight: '18px',
-                letterSpacing: '0px',
-                textAlign: 'left',
-                marginBottom: '16px'
-              }}
-            >
-              {categori}
-            </Typography>
+              <Divider sx={{ width: '296px', margin: 'auto', marginBottom: '16px', height: '4px', borderBottom: '1px solid #090909' }} />
 
-            <Typography
-              sx={{
-                display: 'flex',
-                alignItems: 'center', // Alinea verticalmente el icono con el texto
-                width: 'fit-content',
-                fontFamily: 'Nunito',
-                fontSize: '14px',
-                fontWeight: 400,
-                lineHeight: '20px',
-                letterSpacing: '0px',
-                textAlign: 'left',
-              }}
-            >
-              <LocationOnIcon sx={{ marginRight: '4px', color: '#093C59' }} /> {/* Agrega un margen a la derecha del icono */}
-              {location}
-            </Typography>
+              <Typography color="primary" sx={{ fontWeight: '600', marginBottom: '5px' }}>
+                Más información de interés
+              </Typography>
+              <Typography variant="body2" sx={{ marginBottom: '16px' }}>
+                Nació del sueño de restaurar la salud y adoptar un estilo de vida ideal. Este proyecto familiar creció fundamentado en la permacultura, biodinámica y agroecología, comprometiéndose con la soberanía alimentaria, el bienestar, el regreso al campo, la venta directa y la dignidad de la vida campesina.
+              </Typography>
 
-            <IconButton onClick={toggleExpand}
+              <div style={{ textAlign: 'center' }}>
+                <Button variant="contained" color="primary" sx={{ borderRadius: '100px', padding: '10px 24px', textTransform: 'none' }}>
+                  <Typography sx={{ fontSize: '16px', lineHeight: '20px' }}>Contactar</Typography>
+                </Button>
+              </div>
+            </>
+          )}
+
+          <div style={{ textAlign: 'center' }}>
+            <IconButton
+              onClick={toggleExpand}
+              color='primary'
               sx={{
                 padding: "16px 0px 8px 0px",
-                gap: "16px"
+                textAlign: 'center',
+                marginTop: '8px'
               }}
             >
-              {expanded ? <ExpandMoreIcon /> : <ExpandLessIcon />}
+              {expanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
             </IconButton>
+          </div>
 
-          </CardContent>
         </CardContent>
       </Card>
     </Grid>
-
   );
 }
 
