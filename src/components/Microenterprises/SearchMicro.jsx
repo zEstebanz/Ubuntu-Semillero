378f642e3aeb12478bc4bda26df40aaf189e5575
Esteban <estebanoller73@gmail.com>
2024-03-07 13:33:45 -0300

    update MicroCard and Port

--- src/components/Microenterprises/SearchMicro.jsx
+++ src/components/Microenterprises/SearchMicro.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { TextField, Grid } from '@mui/material';
+import { TextField, Grid, Box } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
-
 const SearchMicro = ({ search, setSearch }) => {
   return (
     <Grid
@@ -9,14 +8,37 @@ const SearchMicro = ({ search, setSearch }) => {
       justifyContent="center"
       className="search-bar"
     >
-      <TextField
-        label="Buscar Microemprendimientos"
-        className="search-input"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          width: '100%',
+          gap: '8px'
+        }}
+      >
+        <SearchIcon color='primary' />
+        <TextField
+          name="message"
+          label=""
+          placeholder="Buscar Microemprendimientos"
+          variant="outlined"
+          fullWidth
+          sx={{
+            '& .MuiOutlinedInput-root': {
+              borderRadius: 0
+            },
+            "& fieldset": {
+              border: 'none'
+            },
+          }}
+        />
+      </Box>
     </Grid>
+
+
   );
 };
 
 export default SearchMicro;
+
+
