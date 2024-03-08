0dc8b14f87f38b20bd5a581032f1e2c7ccf0efd0
Tomas Guignet <tomasguignet9@gmail.com>
2024-03-08 19:35:31 -0300

    Navbar and Drawer styled / DrawerList created

--- src/components/DrawerList.jsx
+++ src/components/DrawerList.jsx
@@ -1,5 +1,42 @@
-function DrawerList() {
-  return <div>DrawerList</div>;
+import { List, ListItem, Typography } from "@mui/material";
+import theme from "../theme/theme";
+import { Link } from "react-router-dom";
+
+function DrawerList({ toggleDrawer }) {
+  const handleClickLink = () => {
+    toggleDrawer();
+  };
+  return (
+    <List>
+      <ListItem>
+        <Link
+          onClick={handleClickLink}
+          to="/"
+          style={{ textDecoration: "none", color: "white" }}
+        >
+          <Typography variant="subtitles">Inicio</Typography>
+        </Link>
+      </ListItem>
+      <ListItem>
+        <Link
+          onClick={handleClickLink}
+          to="/microemprendimientos"
+          style={{ textDecoration: "none", color: "white" }}
+        >
+          <Typography variant="subtitles">Microemprendimientos</Typography>
+        </Link>
+      </ListItem>
+      <ListItem>
+        <Link
+          onClick={handleClickLink}
+          to="publicaciones"
+          style={{ textDecoration: "none", color: "white" }}
+        >
+          <Typography variant="subtitles">Publicaciones</Typography>
+        </Link>
+      </ListItem>
+    </List>
+  );
 }
 
 export default DrawerList;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,42 +5,72 @@ import {
   Drawer,
   Grid,
   IconButton,
+  SwipeableDrawer,
   Toolbar,
   Typography,
 } from "@mui/material";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import logo from "../assets/logoUbuntu.png";
 import { Link } from "react-router-dom";
 import DrawerList from "./DrawerList";
+import CloseIcon from "@mui/icons-material/Close";
+
+import "../assets/styles/NavBarWithDrawer.css";
 
 function NavBar() {
   const [open, setOpen] = useState(false);
+  const navbarRef = useRef();
+  const getNavbarHeight = () => {
+    if (navbarRef.current) {
+      return navbarRef.current.offsetHeight;
+    }
+    return 0; // Valor predeterminado si no se puede obtener la altura del Navbar
+  };
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
   return (
-    <AppBar position="static" color="transparent">
-      <Toolbar>
-        <IconButton
-          edge="start"
-          color="common.black"
-          aria-label="menu"
-          onClick={() => setOpen(open)}
-        >
-          <Menu />
-        </IconButton>
-        <Box
-          display="flex"
-          width="100%"
-          marginRight="1.5rem"
-          justifyContent="center"
-        >
-          <Link to="/">
-            <img src={logo} alt="Logo" height="50" />
-          </Link>
-        </Box>
-      </Toolbar>
-      <Drawer open={open} onClose={() => setOpen(false)}>
-        <DrawerList />
-      </Drawer>
-    </AppBar>
+    <div ref={navbarRef} className={`navbar ${open ? "drawer-open" : ""}`}>
+      <AppBar position="static" color="transparent">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="common.black"
+            aria-label="menu"
+            onClick={toggleDrawer}
+          >
+            {open === true ? <CloseIcon /> : <Menu />}
+          </IconButton>
+          <Box
+            display="flex"
+            width="100%"
+            marginRight="1.5rem"
+            justifyContent="center"
+          >
+            <Link to="/">
+              <img src={logo} alt="Logo" height="50" />
+            </Link>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <SwipeableDrawer
+        variant="persistent"
+        open={open}
+        onOpen={() => setOpen(true)}
+        onClose={() => setOpen(false)}
+        anchor="left"
+        PaperProps={{
+          style: {
+            width: "70%",
+            top: `${getNavbarHeight()}px`,
+            backgroundColor: "#093C59",
+          },
+        }}
+      >
+        <DrawerList toggleDrawer={toggleDrawer} />
+      </SwipeableDrawer>
+    </div>
   );
 }
 
