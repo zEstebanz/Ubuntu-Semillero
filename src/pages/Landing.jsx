d885b46446768fa83df65f4b9bd286382b5b331b
Tomas Guignet <tomasguignet9@gmail.com>
2024-04-01 10:46:34 -0300

    Modified searchBar

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,10 +2,12 @@ import Goals from "../components/landing/goals";
 import Categories from "../components/landing/categories";
 import { Hero } from "../components/landing/Hero";
 import LandingPosts from "../components/landing/LandingPosts";
+import SearchBar from "./../components/SearchBar";
 
 export const Landing = () => {
   return (
     <div>
+      <SearchBar />
       <Hero />
       <Goals />
       <Categories />
