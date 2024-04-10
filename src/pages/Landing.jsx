e68f79658bc5a2da1634ba51690b702576454beb
rodriguez.eric <feric.rodriguez@gmail.com>
2024-04-10 10:53:03 -0300

    fix: Dashboard admin redirection fixed

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,14 +3,9 @@ import Categories from "../components/landing/categories";
 import { Hero } from "../components/landing/Hero";
 import LandingPosts from "../components/landing/LandingPosts";
 import { useSession } from "../hooks/useSession";
-import { Navigate } from "react-router-dom";
 
 export const Landing = () => {
   const user = useSession();
-
-  if(user) {
-    return <Navigate to='/dashboard-admin'/>
-  }
 
   return (
     <div>
